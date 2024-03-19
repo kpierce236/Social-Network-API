8b2238f102950b4f5d2200aa3052c9a61f8b1d2a
Kevin Pierce <kpierce236@gmail.com>
2024-03-19 19:34:43 -0400

    created user model and schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,41 @@
+// models/User.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// Virtual for friendCount
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
