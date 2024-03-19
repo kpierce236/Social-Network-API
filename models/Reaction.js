a60dbe71b95d6b824be16142999937ed017e5c68
Kevin Pierce <kpierce236@gmail.com>
2024-03-19 19:51:51 -0400

    created reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,26 @@
+// models/Reaction.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+});
+
+module.exports = reactionSchema;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,5 @@
+// models/index.js
+const User = require('./User');
+const Thought = require('./Thought');
 
+module.exports = { User, Thought };
