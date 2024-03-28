85055bc825471ad70c9cb7d946036d61d8139a59
Kevin Pierce <kpierce236@gmail.com>
2024-03-28 00:56:19 -0400

    Syntax error

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require('dotenv').config
+require('dotenv').config()
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
